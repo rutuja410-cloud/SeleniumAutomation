6d0472b287f5834409fc901e86dd978cca669e30
my name <rutuja.adamapure97@gmail.com>
2025-12-25 20:47:10 +0530

    my first commit

---
+++ src/test/java/BrowserLonch.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class BrowserLonch
+{
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup()
+    {
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/v1/index.html");
+    }
+
+    @Test
+    public void login()
+    {
+
+    }
+
+    @AfterMethod
+    public void logout()
+    {
+       // driver.quit();
+    }
+}
